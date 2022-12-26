cce9fcf33f855f9fb98cefed530c1f49c4594863
hlleung74 <hlleung74-c@my.cityu.edu.hk>
2022-12-27 05:53:59 +0800

    Change php content

--- contact.php
+++ contact.php
@@ -1,16 +1,20 @@
 <?php
 
-$name = $_POST['name'];
-$mailFrom = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$mailFrom = $_POST['email'];
+	$phone = $_POST['phone'];
+	$message = $_POST['message'];
 
-$mailTo = "hl.kessieleung@gmail.com";
-$mailheader = "From:""<".$mailFrom;
-$subject = "Message from Contact Form";
-$txt = "You have received an email from " .$name. ".\n\n" .$phone. ".\n\n" .$message;
+	$mailTo = "hl.kessieleung@gmail.com";
+	$mailheader = "From:"
+	$mailheader .= $mailFrom;
+	$subject = "Message from Contact Form";
+	$txt = "You have received an email from ";
+	$txt .= $name;
 
-mail($mailTo, $subject, $message, $mailheader) or die("Error!");
-header('Location: ../');
+	mail($mailTo, $subject, $txt, $mailheader) or die("Error!");
+	header('Location: ../');
+}
 
 ?>
