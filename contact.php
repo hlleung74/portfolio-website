3d47416788dc8da2f41b3ed729687d2d4c523ed1
hlleung74 <hlleung74-c@my.cityu.edu.hk>
2022-12-27 05:23:09 +0800

    Second Commit

--- contact.php
+++ contact.php
@@ -1,15 +1,14 @@
 <?php
 
-
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$subject = $_POST['subject'];
 $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email."<".$phone.">\r\n";
 
 $recipient = "hl.kessieleung@gmail.com";
+$subject = "Message from Contact Form";
 
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
