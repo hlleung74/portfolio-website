f9627617633533a1d7d686f517bc0028e7a4e8e0
hlleung74 <hlleung74-c@my.cityu.edu.hk>
2022-12-27 15:03:22 +0800

    Edit Contact Form

--- contact.php
+++ contact.php
@@ -1,135 +1,53 @@
 <?php
- 
-if(isset($_POST['email'])) {
- 
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
- 
-    $email_to = "hl.kessieleung@gmail.com";
- 
-    $email_subject = "Contact Form Submission";
- 
-     
- 
-     
- 
-    function died($error) {
- 
-        // your error code can go here
- 
-        echo "<h1>Whoops!</h1><h2>There appears to be something wrong with your completed form.</h2>";
- 
-        echo "<strong><p>The following items are not specified correctly.</p></strong><br />";
- 
-        echo $error."<br /><br />";
- 
-        echo "<p>Return to the form and try again.</p><br />";
-		echo "<p><a href='index.php'>return to the homepage</a></p>";
-        die();
-		
- 
-    }
-  
- 
-    $first_name = $_POST['first_name']; // required
- 
-    $last_name = $_POST['last_name']; // required
- 
-    $email_from = $_POST['email']; // required
- 
-    $telephone = $_POST['telephone']; // not required
- 
-    $comments = $_POST['comments']; // required
- 
-     
- 
-    $error_message = "";
- 
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
- 
-    $error_message .= '<li><p>The completed e-mail address appears to be incorrect<p></li>';
- 
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$first_name)) {
- 
-    $error_message .= '<li><p>First name appears to be wrong</p></li>';
- 
-  }
- 
-  if(!preg_match($string_exp,$last_name)) {
- 
-    $error_message .= '<li><p>Last name appears to be wrong</p></li>';
- 
-  }
- 
-  if(strlen($comments) < 2) {
- 
-    $error_message .= '<li><p>Message appears to be incorrect</p></li>';
- 
-  }
- 
-  if(strlen($error_message) > 0) {
- 
-    died($error_message);
- 
-  }
- 
-    $email_message = "Form details are given below.\n\n";
- 
-     
- 
-    function clean_string($string) {
- 
-      $bad = array("content-type","bcc:","to:","cc:","href");
- 
-      return str_replace($bad,"",$string);
- 
-    }
- 
 
- 
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
- 
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
- 
-    $email_message .= "Email Adress: ".clean_string($email_from)."\n";
- 
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
- 
-    $email_message .= "Comments: ".clean_string($comments)."\n";
- 
-      
- 
-// create email headers
- 
-$headers = 'From: '.$email_from."\r\n".
- 
-'Reply-To: '.$email_from."\r\n" .
- 
-'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers);  
- 
-?>
- 
+// Using $_POST function to send data 
 
- 
-<!-- include your own success html here -->
- 
- 
- 
-<h1>Thank you for your message!</h1> <h2>We will contact you as soon as possible.</h2>
+// isset() function used to check if submit button is clicked
 
- 
- 
- 
-<?php
- 
+if (isset($_POST['submit'])) {
+
+// Grabbing elements by targeting 'name' attribute
+  $name = $_POST['name'];
+  $email = $_POST['emailAddress'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+  $recipient = "hl.kessieleung@gmail.com";
+
+// using empty() function to check if fields are empty
+
+  if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+// Changing URL 
+    //header("Location: index.php?form=empty");
+    echo "Please fill in any missing fields!";
+      } 
+
+      else {
+
+// Checking if email is valid with filter_var() function 
+// FILTER_VALIDATE_EMAIL used as parameter to check email validity
+
+if(!filter_var($email, FILTER_VALIDATE_EMAIL ))  {
+  //header("Location: index.php?invalidemail");
+  echo "Please enter a valid email address.";
 }
- 
+      // Send the email if no errors are found
+      else {
+
+// mail() function allows for the sending of emails
+// Three parameters must be passed for the mail() function to work.
+// 1. Email address of the recipient
+// 2. The subject line of the email
+// 3. The message
+
+mail($recipient, $subject, $message);
+
+
+header("Location: contact.php?mailsend");
+      }
+    }
+
+
+
+}
+
 ?>
